f26fc9d1e9504761eef68212b3dfbcae12764381
Joboing <54768963+Joboing@users.noreply.github.com>
2020-05-15 11:28:06 +0800

    Update handle_rotated_image.py

--- handle_rotated_image.py
+++ handle_rotated_image.py
@@ -1,9 +1,10 @@
 import cv2
 from math import *
 import numpy as np
+import os
 
 
-def rotate(img):
+def rotate(img, save_path):
     img = cv2.imread(img)
     # img = Image.open(pic)
     img = np.array(img)
@@ -23,9 +24,11 @@ def rotate(img):
     # matRotation[1, 2] += (heightNew - height) / 2  # 重点在这步
     # 图片上的一个点(matRotation[0][0] * x + matRotation[0][1] * y + matRotation[0][2], matRotation[1][0] * x + matRotation[1][1] * y + matRotation[1][2] )
     imgRotation = cv2.warpAffine(img, matRotation, (widthNew, heightNew), borderValue=(255, 255, 255))  # 进行仿射变化
+    
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    cv2.imwrite(save_path+"imgname", imgRotation)
 
-    cv2.imwrite("D:\\data\\image_retrieval\\data\\mater_videos\\msdk\\303\\4774\\5\\666.jpg", imgRotation)
-
-
-rotate(
-    "D:\\data\\image_retrieval\\data\\mater_videos\\msdk\\303\\4774\\5\\303_4774_5_14_200\\158172004370890673-00dd9366f6e49ad68ef86bfaba8228f8-1-0-1589432329282.jpg")
+file_dir = "image_path"
+save_path = "save_rotated_path"
+rotate(file_dir, save_path)
